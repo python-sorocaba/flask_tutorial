f056278d3c55fcce765d5a387908a4db2c4ea7b7
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-08-01 23:09:57 -0300

    Add series and list series (on home page)

--- tvseries/core.py
+++ tvseries/core.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+series = []
 
 
 @app.route('/')
 @app.route('/<name>')
 def home(name=None):
-    return render_template('home.html', name=name)
+    return render_template('home.html', name=name, series=series)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        serie_name = request.form.to_dict().get('serie-name')
+        series.append(serie_name)
+        return redirect('/')
+
+    return render_template('add.html')
--- tvseries/tests.py
+++ tvseries/tests.py
@@ -18,3 +18,15 @@ class TestCore:
         response = self.client.get("/Rafael")
         assert '<h1>Hello Rafael!</h1>' in response.data.decode('utf-8')
         assert response.status_code == 200
+
+    def test_get_add(self):
+        response = self.client.get("/add")
+        assert ('<input type="text" name="serie-name" id="id_serie-name">' in
+                response.data.decode('utf-8'))
+        assert response.status_code == 200
+
+    def test_post_add(self):
+        response = self.client.post("/add", data={"serie-name": "Teste"})
+        from tvseries.core import series
+        assert series == ['Teste']
+        assert response.status_code == 302
