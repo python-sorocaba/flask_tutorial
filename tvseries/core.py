0c29f934fced341f480a8cabf106b2f57490f077
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-08-01 22:43:19 -0300

    Improve hello world

--- tvseries/core.py
+++ tvseries/core.py
@@ -4,10 +4,6 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template('home.html')
-
-
 @app.route('/<name>')
-def name(name):
+def home(name=None):
     return render_template('home.html', name=name)
