65c747f203cf745d68f43337cdb39e4de6121c3d
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-08-01 22:33:57 -0300

    Basic template inheritance and beautify pages

--- tvseries/tests.py
+++ tvseries/tests.py
@@ -11,10 +11,10 @@ class TestCore:
 
     def test_get_home(self):
         response = self.client.get("/")
-        assert response.data.decode('utf-8') == '<h1>Hello world!</h1>'
+        assert '<h1>Hello world!</h1>' in response.data.decode('utf-8')
         assert response.status_code == 200
 
     def test_get_name(self):
         response = self.client.get("/Rafael")
-        assert response.data.decode('utf-8') == '<h1>Hello Rafael!</h1>'
+        assert '<h1>Hello Rafael!</h1>' in response.data.decode('utf-8')
         assert response.status_code == 200
