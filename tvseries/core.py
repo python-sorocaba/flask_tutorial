63741c0c2ced5d5950169ddc1c451d565af6446e
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-08-13 23:06:02 -0300

    Add TVSerie model
    - add TVSerie model to persist TV Series
    - change structure of tests to create tests to model
    - change .travis.yml to call py.test more easily
    - add ignore (.gitignore) to database files of sqlite3

--- tvseries/core.py
+++ tvseries/core.py
@@ -2,8 +2,11 @@ import os
 from random import choice
 
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tvseries.sqlite3'
+db = SQLAlchemy(app)
 series = []
 
 
@@ -22,3 +25,24 @@ def add():
         return redirect('/')
 
     return render_template('add.html')
+
+
+class TVSerie(db.Model):
+    __table_args__ = {'sqlite_autoincrement': True}
+    id = db.Column(db.BigInteger().with_variant(db.Integer, "sqlite"),
+                   nullable=False, unique=True,
+                   autoincrement=True, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    episodies_number = db.Column(db.Integer, nullable=False, default=1)
+    author = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        if self.description:
+            self.description = "{0}...".format(self.description[0:10])
+
+        return ("TVSerie(id={!r}, name={!r}, "
+                "description={!r}, episodies_number={!r})").format(
+            self.id, self.name,
+            self.description,
+            self.episodies_number)
---
+++ tvseries/tests/test_core.py
@@ -0,0 +1,89 @@
+import pytest
+
+from tvseries.core import TVSerie
+
+
+@pytest.mark.usefixtures('client_class')
+class TestCore:
+
+    @pytest.fixture
+    def app(self):
+        from tvseries.core import app
+        return app
+
+    def test_get_home(self):
+        response = self.client.get("/")
+        assert '<div class="banner">' in response.data.decode('utf-8')
+        assert response.status_code == 200
+
+    def test_get_add(self):
+        response = self.client.get("/add")
+        assert ('<input type="text" name="serie-name" id="id_serie-name">' in
+                response.data.decode('utf-8'))
+        assert response.status_code == 200
+
+    def test_post_add(self):
+        response = self.client.post("/add", data={"serie-name": "Teste"})
+        from tvseries.core import series
+        assert series == ['Teste']
+        assert response.status_code == 302
+
+    @pytest.fixture
+    def db(self, app, request):
+        from tvseries.core import db as db_test
+
+        def teardown():
+            db_test.drop_all()
+
+        db_test.app = app
+        db_test.create_all()
+
+        request.addfinalizer(teardown)
+        return db_test
+
+    def test_insert_on_model_tvserie(self, db):
+        description = (
+            "Há muito tempo, em um tempo esquecido, uma força "
+            "destruiu o equilíbrio das estações. Em uma terra "
+            "onde os verões podem durar vários anos e o inverno "
+            "toda uma vida, as reivindicações e as forças sobrenaturais "
+            "correm as portas do Reino dos Sete Reinos. A irmandade "
+            "da Patrulha da Noite busca proteger o reino de cada "
+            "criatura que pode vir de lá da Muralha, mas já não tem "
+            "os recursos necessários para garantir a segurança de "
+            "todos. Depois de um verão de dez anos, um inverno "
+            "rigoroso promete chegar com um futuro mais sombrio. "
+            "Enquanto isso, conspirações e rivalidades correm no jogo "
+            "político pela disputa do Trono de Ferro, o símbolo do "
+            "poder absoluto."
+        )
+        serie = TVSerie(name="Game of Thrones",
+                        description=description,
+                        author="George R.R. Martin")
+        db.session.add(serie)
+        db.session.commit()
+        assert TVSerie.query.count() == 1
+
+    def test_repr_on_model_tvserie(self, db):
+        description = (
+            "Há muito tempo, em um tempo esquecido, uma força "
+            "destruiu o equilíbrio das estações. Em uma terra "
+            "onde os verões podem durar vários anos e o inverno "
+            "toda uma vida, as reivindicações e as forças sobrenaturais "
+            "correm as portas do Reino dos Sete Reinos. A irmandade "
+            "da Patrulha da Noite busca proteger o reino de cada "
+            "criatura que pode vir de lá da Muralha, mas já não tem "
+            "os recursos necessários para garantir a segurança de "
+            "todos. Depois de um verão de dez anos, um inverno "
+            "rigoroso promete chegar com um futuro mais sombrio. "
+            "Enquanto isso, conspirações e rivalidades correm no jogo "
+            "político pela disputa do Trono de Ferro, o símbolo do "
+            "poder absoluto."
+        )
+        serie = TVSerie(name="Game of Thrones",
+                        description=description,
+                        author="George R.R. Martin")
+        assert repr(serie) == (
+            "TVSerie(id=None, name='Game of Thrones', "
+            "description='Há muito t...', episodies_number=None)"
+        )
