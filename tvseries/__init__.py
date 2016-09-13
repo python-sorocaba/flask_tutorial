6b93cff189bb7ae957c0cdb37edd93cf24491e37
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-09-12 23:01:34 -0300

    Problem: Hen and egg (python circular imports)

--- tvseries/__init__.py
+++ tvseries/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from tvseries import config
 
+print("Problem: Hen and egg (python circular imports)")
 app = Flask(__name__)
 app.config.from_object(config.DevelopmentConfig)
 
