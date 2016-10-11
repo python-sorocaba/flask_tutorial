cac4c1cfd44b867c10a67eb2220c3809eef0efc3
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-10-10 21:43:32 -0300

    Add Flask-Collect on manage

--- manage.py
+++ manage.py
@@ -1,9 +1,20 @@
 from flask_script import Manager
+from flask_collect import Collect
+
 from tvseries import create_app
 from tvseries.config import DevelopmentConfig
 
 app = create_app(config=DevelopmentConfig)
 manager = Manager(app)
+
+collect = Collect()
+collect.init_app(app)
+
+
+@manager.command
+def collect():
+    """Collect static from blueprints."""
+    return app.extensions['collect'].collect()
 
 if __name__ == "__main__":
     manager.run()
--- tvseries/config.py
+++ tvseries/config.py
@@ -1,12 +1,16 @@
+import os
 from decouple import config
 
 
 class BaseConfig(object):
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     SITE_NAME = 'Flask Tutorial'
     SECRET_KEY = config('SECRET_KEY')
     SERVER_NAME = config('SERVER_NAME')
     SQLALCHEMY_DATABASE_URI = config('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    COLLECT_STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    COLLECT_STORAGE = 'flask_collect.storage.file'
     DEBUG = False
     TESTING = False
 
